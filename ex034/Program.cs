c0f0a4e3e763ff305b4fab90d0acbb167c3ee12e
Maxim <maks.bidenko.96@mail.ru>
2022-08-20 12:48:02 +0300

    Сделал 34 задачу

---
+++ ex034/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+int[] myArray = CreateRandomArray(45, 100, 1000);
+int count = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    
+    int num = myArray[i];
+    if (num % 2 == 0)
+    {
+        count++;
+    }
+}
+ShowArray(myArray); 
+Console.Write(count);
+
