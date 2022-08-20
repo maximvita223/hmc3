2fd6186a9b407724355e46ca61a12af9c186b974
Maxim <maks.bidenko.96@mail.ru>
2022-08-20 12:55:06 +0300

    Сделал 36 задачу

---
+++ ex036/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+
+int[] myArray = CreateRandomArray(6, 1, 1000);
+ShowArray(myArray);
+int sum = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (i % 2 == 0) 
+    {
+        sum += myArray[i]; 
+    }
+}
+Console.WriteLine(sum);
+
